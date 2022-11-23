fb51719cfffc53d8463e59251d2dfbba1e8f307f
Nitin308 <114205043+Nitin308@users.noreply.github.com>
2022-11-23 13:49:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Game !!");
+
+            int PlayerPosition = 0;
+            int diceRoll;
+
+            Console.WriteLine("\nPress ENTER to Play the game");
+            string play = Console.ReadLine();
+            Console.WriteLine($"Player is at position {PlayerPosition}");
+
+            Random random = new Random();
+            diceRoll = random.Next(1, 7);
+            Console.WriteLine($"\nPlayer rolled number {diceRoll}");
+        }
+    }
+}
